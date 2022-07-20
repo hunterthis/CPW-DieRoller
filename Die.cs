47a9066e7c0eca73e23c924c14a521f9fa7c8642
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-07-19 17:06:10 -0700

    Added commentary on Die class

--- Die.cs
+++ Die.cs
@@ -11,12 +11,22 @@ namespace DieRollerA
     /// </summary>
     public class Die
     {
+        /// <summary>
+        ///  current face up value of the die
+        /// </summary>
     public byte FaceValue { get; set; }
-    
+    /// <summary>
+    /// True if die is currently held
+    /// </summary>
     public bool IsHeld { get; set; }
-    
+    /// <summary>
+    /// Rolls die and sets the <see cref="FaceValue"/>
+    /// to the new number
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     public void Roll()
         {
+            // generate random number, sets face value and returns new number
             throw new NotImplementedException();
         }
     }
