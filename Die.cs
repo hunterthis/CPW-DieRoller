a7feb0630271895550142c5b1a1d94d6008f92bd
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-07-19 17:02:59 -0700

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRollerA
+{
+    /// <summary>
+    /// represents six sided dice in game
+    /// </summary>
+    public class Die
+    {
+    public byte FaceValue { get; set; }
+    
+    public bool IsHeld { get; set; }
+    
+    public void Roll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
